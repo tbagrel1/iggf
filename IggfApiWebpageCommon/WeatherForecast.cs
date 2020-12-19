12cbf00e0ededd1a9a5154d3c1f609e048df9c84
Thomas BAGREL <tomsb07@gmail.com>
2020-12-19 12:20:56 +0100

    basic structure

--- IggfApiWebpageCommon/WeatherForecast.cs
+++ IggfApiWebpageCommon/WeatherForecast.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace IggfCommon
+namespace IggfApiWebpageCommon
 {
     public class WeatherForecast
     {
--- IggfScoreApi/Controllers/WeatherForecastController.cs
+++ IggfScoreApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,4 @@
-﻿using IggfCommon;
+﻿using IggfApiWebpageCommon;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
